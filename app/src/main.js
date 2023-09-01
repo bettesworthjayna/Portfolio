5386c8554913e3c1a81af04936ef569cb1cce83b
Jayna Bettesworth <bettesworthjayna@gmail.com>
2023-09-01 10:37:11 -0700

    adding new dependencies

--- app/src/main.js
+++ app/src/main.js
@@ -4,4 +4,15 @@ import App from './App.vue'
 import router from './router/router'
 import './index.css'
 
-createApp(App).use(router).mount('#app')
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+const app = createApp(App)
+app.use(router)
+app.use(BootstrapVue)
+app.use(IconsPlugin)
+app.mount('#app')
